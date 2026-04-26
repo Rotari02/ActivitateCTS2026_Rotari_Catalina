d8b01ed24956032919f1e896719e56e2cad050db
Rotari02 <catalina.rotari02@gmail.com>
2026-04-27 00:00:28 +0300

    Seminar 9 Facade si Adapter

--- LucruIndividual/PregatireTest1Creationale/MasinaSingletonSimpleFactory/src/SimpleFactory/FabricaMasini/Main.java
+++ LucruIndividual/PregatireTest1Creationale/MasinaSingletonSimpleFactory/src/SimpleFactory/FabricaMasini/Main.java
@@ -3,10 +3,10 @@ package SimpleFactory.FabricaMasini;
 import SimpleFactory.clase.AMasina;
 
 public class Main {
-    public static void main(String[] args) {
-        FabricaMasina fabrica =new FabricaMasina();
-        AMasina masina1= fabrica.creeazaMasina(TipMasina.SEDAN,"A123FGJ","HFGH",23456.00);
-    }
+//    public static void main(String[] args) {
+//        FabricaMasina fabrica =new FabricaMasina();
+//        AMasina masina1= fabrica.creeazaMasina(TipMasina.SEDAN,"A123FGJ","HFGH",23456.00);
+//    }
 
 
 
